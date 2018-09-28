7eddb41d8f4be1646417fdf3015a344bdbb87921
matackett <maria.tackett@duke.edu>
2018-09-27 21:44:31 -0400

    Rewriting documentation using devtools::document() functionality.

---
+++ STA210/R/prostate.R
@@ -0,0 +1,17 @@
+#' Identifying Risk Factors of Prostate Cancer
+#'
+#' @description Risk factors of prostate cancer in 67 men. 
+#'
+#' @format A data frame with 67 observations and 3 variables.
+#'   \describe{
+#'      \item{\code{age}}{Age in years}
+#'      \item{\code{cavol}}{Cancer volume - percentage of prostate taken up by the tumor.}
+#'      \item{\code{psa}}{Prostate specific antigen - an antigenic enzyme released by the prostate and found in abnormally high concentrations in the blood of men with prostate cancer. Measured in nanograms per milliliter (ng/mL) of blood.}
+#' }
+#'
+#' @source Data obtained from the "Elements of Statistical Learning" data sets webpage \url{https://web.stanford.edu/~hastie/ElemStatLearn/data.html}
+#' @source Stamey, T., Kabalin, J., McNeal, J., Johnstone, I., Freiha, F., Redwine, E. and Yang, N. (1989). Prostate specific antigen in the diagnosis and treatment of adenocarcinoma of the prostate II radical prostatectomy treated patients, Journal of Urology 16: 1076–1083.
+
+"prostate"
+
+
---
+++ STA210/prostate.R
@@ -0,0 +1,14 @@
+#' Detecting Risk Factors of Prostate Cancer 
+#'
+#' @source @source Data obtained from the "Elements of Statistical Learning" data sets webpage \url{https://web.stanford.edu/~hastie/ElemStatLearn/data.html} \cr
+#' Stamey, T., Kabalin, J., McNeal, J., Johnstone, I., Freiha, F., Redwine, E. and Yang, N. (1989). Prostate specific antigen in the diagnosis and treatment of adenocarcinoma of the prostate II radical prostatectomy treated patients, Journal of Urology 16: 1076–1083.
+#' @format A data frame with columns:
+#' \describe{
+#'   \item{\code{age}}{Age in years}
+#'   \item{\code{cavol}}{Cancer volume - percentage of prostate taken up by the tumor.}
+#'   \item{\code{psa}}{Prostate specific antigen - an antigenic enzyme released by the prostate and found in abnormally high concentrations in the blood of men with prostate cancer. Measured in nanograms per milliliter (ng/mL) of blood.}
+#' }
+#' @description Identifying risk factors of prostate cancer
+
+"prostate"
+
